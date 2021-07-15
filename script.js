26eeee96bce001107fcc1f94c41fecc3ad02b12b
Nam Ant <83125103+doonamm@users.noreply.github.com>
2021-07-15 15:48:46 +0700

    Add files via upload

---
+++ script.js
@@ -0,0 +1,31 @@
+const font_size = window.getComputedStyle(document.querySelector('.fa-snowflake')).getPropertyValue('font-size');
+setInterval(createSnowFlake, Math.floor(Math.random()*100) + 70)
+
+var isFullScreen = false;
+
+document.addEventListener('dblclick', ()=>{
+    if(!isFullScreen)
+        document.documentElement.requestFullscreen();
+    else
+        document.exitFullscreen();
+    isFullScreen = !isFullScreen;
+})
+
+function createSnowFlake(){
+    const snow_flake = document.createElement('i');
+
+    snow_flake.classList.add('fas', 'fa-snowflake');
+
+    const randomTime = Math.round(Math.random()*5) + 4;
+
+    snow_flake.style.left = Math.random()*document.body.clientWidth + 'px';
+    snow_flake.style.animationDuration = randomTime + 's';
+    snow_flake.style.opacity = Math.random();
+    snow_flake.style.fontSize = Math.round(Math.random()*10) - 5 + parseInt(font_size) + 'px';
+
+    document.body.appendChild(snow_flake);
+
+    setTimeout(()=>{
+        snow_flake.remove();
+    }, randomTime*1000);
+}
